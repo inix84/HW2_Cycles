00fd7314e4b325a06a914954a0636fc224f33261
inix84 <inix84@mail.ru>
2024-10-13 14:20:23 +0500

    8 задач

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,92 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 1. С помощью цикла for выведите " +
-                "в консоль все целые числа от 1 до 10:");
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        System.out.println("Задача 1. Копим 2 459 000 руб c помощью цикла ПОКА:");
+        int i = 0; // инвариант, начинаем копить с нуля
+        int n = 0; // счетчик месяцев
+        while (i <= 2459000) {
+            i = i + 15000;
+            n++;
+        }
+        System.out.println("Месяц " + n + " сумма накоплений равна " + i + " рублей.");
+        System.out.println();
+        System.out.println("Задача 2. В двух строках через пробел числа от 1 до 10 и от 10 до 1:");
+        int q = 1;
+        while (q <= 10) {
+            System.out.print(q + " ");
+            q++;
+        }
+        System.out.print(".");
+        System.out.println();
+        while (q > 1) { // на данном шаге q=11
+            q--;
+            System.out.print(q + " ");
+        }
+        System.out.print(".");
+        System.out.println();
+        System.out.println();
+        System.out.println("Задача 3. Рождаемость:");
+        float y = 12_000_000; // В стране Y население — 12 миллионов человек
+        int w = 0; // счетчик годов, 10 лет нужно
+        float k; // коэффициент 12 млн : 1 тыс
+        while (w < 10) {
+            w++;
+            k = y / 1000; // расчет коэффициента на 1000 населения
+            y = y + 17 * k - 8 * k;
+            System.out.println("Год " + w + " - численность населения составляет " + (int) y + ".");
         }
         System.out.println();
-        System.out.println("Задача 2. С помощью цикла for выведите " +
-                "в консоль все целые числа от 10 до 1:");
-        for (int j = 10; j >= 1; j--) {
-            System.out.println(j);
+        System.out.println("Задача 4. Копим Василию 12 млн:");
+        float e = 15_000; // инвариант, начинаем копить 15000 рублей
+        float r = 1.07f; // 7% от банка
+        int t = 0; // счетчик месяцев
+        while (e < 12_000_000) {
+            t++;
+            e = e * r;
+            System.out.println("Через " + t + " месяц(ев), сумма накоплений равна " + (int) e + " рублей.");
         }
         System.out.println();
-        System.out.println("Задача 3. Выведите в " +
-                "консоль все четные числа от 0 до 17:");
-        for (int q = 0; q < 17; q = q + 2) {
-            System.out.println(q);
+        System.out.println("Задача 5. Копим Василию 12 млн: (СОКР.ВЕРСИЯ)");
+        float a = 15_000; // инвариант, начинаем копить 15000 рублей
+        float s = 1.07f; // 7% от банка
+        int d = 0; // счетчик месяцев
+        while (a < 12_000_000) {
+            d++;
+            a = a * s;
+            if (d % 6 == 0) {
+                System.out.println("Через " + d + " месяц(ев), сумма накоплений равна " + (int) a + " рублей.");
+            }
+        }
+        System.out.println("(Если Василию интересно, то) ИТОГ: Через " + d + " месяц(ев), сумма накоплений равна " + (int) a + " рублей.");
+        System.out.println();
+        System.out.println("Задача 6. Василий & 9 лет банка:");
+        float z = 15_000; // инвариант, начинаем копить 15000 рублей
+        float x = 1.07f; // 7% от банка
+        int v = 9; // срок 9 лет
+        int c = 0; // счетчик месяцев
+        while (c < 12 * v) {
+            c++;
+            z = z * x;
+            if (c % 6 == 0) {
+                System.out.println("Через " + c + " месяц(ев), сумма накоплений равна " + (int) z + " рублей.");
+            }
         }
         System.out.println();
-        System.out.println("Задача 4. Выведите в консоль все числа " +
-                "от 10 до −10 от бо́льшего числа к меньшему:");
-        for (int w = 10; w >= -10; w--) {
-            System.out.print(w + " ");
+        System.out.println("Задача 7. Пятница — отчетный день:");
+        int u = 5; //
+        while (u < 31) {
+            System.out.println("Сегодня пятница, " + u + "-е число. Необходимо подготовить отчет!!!");
+            u = u + 7;
         }
         System.out.println();
-        System.out.println("Задача 5. Високосные года, начиная с 1904 года до 2096:");
-        for (int e = 1904; e <= 2096; e = e + 4) {
-            System.out.println(e + " год является високосным");
-        }
-        System.out.println();
-        System.out.println("Задача 6. Напишите программу, которая выводит " +
-                "в консоль последовательность чисел c 7 до 98:");
-        for (int r = 7; r <= 98; r = r + 7) {
-            System.out.print(r + " ");
-        }
-        System.out.println();
-        System.out.println("Задача 7. Напишите программу, которая " +
-                "выводит в консоль последовательность чисел c 1 до 512:");
-        for (int t = 1; t <= 512; t = t * 2) {
-            System.out.print(t + " ");
-        }
-        System.out.println();
-        System.out.println("Задача 8. Про сумму годовых накоплений в реальной банке:");
-        int total = 0; // инвариант накоплений
-        for (int y = 1; y <= 12; y++) { // переменная будет месяцем
-            total = total + 29000;
-            System.out.println("Месяц " + y + ", сумма накоплений равна " + total + " рублей");
-        }
-        System.out.println();
-        System.out.println("Задача 9. Про сумму годовых накоплений в коммерческом банке:");
-        float total2 = 0; // инвариант накоплений
-        for (int u = 1; u <= 12; u++) { // переменная будет месяцем
-            total2 = total2 + 29000; // сумма пролежала месяц
-            total2 = total2 * (float) 1.01; // и с нее добавили 1 %
-            System.out.println("Месяц " + u + ", сумма накоплений равна " + total2 + " рублей");
-        }
-        System.out.println();
-        System.out.println("Задача 10. Напишите программу, " +
-                "которая выводит в консоль таблицу умножения на 2:");
-        for (int o = 1; o <= 10; o++) {
-            System.out.println("2*"+o+"="+2*o);
+        System.out.println("Задача 8. Астрономическое приложение:");
+        int year = 2024;
+        int currentYear = year - 200; // инвариант изменяющегося года
+        int year100 = year + 100;
+        while (currentYear <= year100) {
+            currentYear++;
+            if (currentYear % 79 == 0) {
+                System.out.println(currentYear);
+            }
         }
     }
 }
